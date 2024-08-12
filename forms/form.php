1a4973e2ad5530cae1ceb41386ee70d06ad2e6e6
BHAVYA <bhavyaspatel04@gmail.com>
2024-08-12 16:10:26 +0530

    first commit

---
+++ forms/form.php
@@ -0,0 +1,33 @@
+<?php
+
+if(isset($_POST['btn-send']))
+{
+    $name = $_POST['UName']; // Changed 'name' to 'UName' to match form input name
+    $email = $_POST['Email'];
+    $subject = $_POST['Subject'];
+    $msg = $_POST['msg']; // Added this line to get the message from the form
+
+    if(empty($name) || empty($email) || empty($subject) || empty($msg))
+    {
+        header("Location: index.php?error");
+    }
+    else
+    {
+        $to = 'patelbhavya744@gmail.com';
+        $headers = "From: ".$email;
+
+        if(mail($to, $subject, $msg, $headers))
+        {
+            header("Location: index.php?success");
+        }
+        else
+        {
+            header("Location: index.php?error");
+        }
+    }
+}
+else
+{
+    header("Location: index.php");
+}
+?>
